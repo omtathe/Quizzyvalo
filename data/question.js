937ccdd1b43d7f584d753263f05f4554fe94386e
Vinit-ingale <inglevinit84@gmail.com>
2026-03-11 10:54:29 +0530

    adding temp images to folder to make prototype

---
+++ data/question.js
@@ -0,0 +1,25 @@
+export const quest=[{
+      type:'agent',
+      question:'which agents have smoke of highest duration?',
+      option1:'Omen',
+      option2:'Brimstone',
+      option3:'Viper',
+      option4:'Astra',
+      answer:'Brimstone'
+},{
+      type:'agent',
+      question:'which agent have ability of teleportation?',
+      option1:'Omen',
+      option2:'Brimstone',
+      option3:'Viper',
+      option4:'Astra',
+      answer:'Brimstone'
+},{
+      type:'agent',
+      question:'who is this agent? ',
+      option1:'Omen',
+      option2:'Brimstone',
+      option3:'Viper',
+      option4:'Astra',
+      answer:'Brimstone'
+},]
